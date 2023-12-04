23c03568fd0385c987314bbcf57a4513922d0230
VictorMurigu <77755334+VictorMurigu@users.noreply.github.com>
2023-12-04 03:12:11 +0300

    Update Intro.js

--- src/Components/Intro.js
+++ src/Components/Intro.js
@@ -22,7 +22,7 @@ const Intro = () => {
                 
             </div>
             <div>
-                <img src="./images/myimg1.jpg" alt="profile" className="profileImg"/>
+                <img src="./images/myimg2.jpg" alt="profile" className="profileImg"/>
 
             </div>
             
